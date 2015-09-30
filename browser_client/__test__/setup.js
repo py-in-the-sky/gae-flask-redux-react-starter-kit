833d3e43b4129acb814aac19fd2cbaa3d53babad
Ryan Wise <ryanmwise1@gmail.com>
2015-09-30 08:41:02 -0700

    update jsdom to work with Node 4.1.1
    also got rid of the unused mocha-jsdom

--- browser_client/__test__/setup.js
+++ browser_client/__test__/setup.js
@@ -10,30 +10,30 @@ console.log('\n--- MOCHA SETUP START ---')
 // "React seems to expect that, if you use `setState`, DOM is available.
 // To work around the issue, we use jsdom so React doesn’t throw [an]
 // exception when DOM isn’t available."
-if (global.document === undefined) {
-    global.document  = jsdom('<!doctype html><html><body></body></html>');
-    global.window    = document.parentWindow;
-    global.navigator = { userAgent: 'node.js' };
+if (GLOBAL.document === undefined) {
+    GLOBAL.document  = jsdom('<!doctype html><html><body></body></html>');
+    GLOBAL.window    = document.defaultView;
+    GLOBAL.navigator = { userAgent: 'node.js' };
     console.log('`jsdom` instance created for testing React components')
 }
 else
-    console.log('`global.document` already taken');
+    console.log('`GLOBAL.document` already taken');
 
 
-if (global.expect === undefined) {
-    global.expect = expect;
-    console.log('`expect` set as a global')
+if (GLOBAL.expect === undefined) {
+    GLOBAL.expect = expect;
+    console.log('`expect` set as a GLOBAL')
 }
 else
-    console.log('`global.expect` already taken');
+    console.log('`GLOBAL.expect` already taken');
 
 
-if (global.__DEV__ === undefined) {
-    global.__DEV__ = false;
-    console.log('`__DEV__ = false` set as a global')
+if (GLOBAL.__DEV__ === undefined) {
+    GLOBAL.__DEV__ = false;
+    console.log('`__DEV__ = false` set as a GLOBAL')
 }
 else
-    console.log('`global.__DEV__` already taken');
+    console.log('`GLOBAL.__DEV__` already taken');
 
 
 console.log('--- MOCHA SETUP END ---\n')
