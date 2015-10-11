6a715e2efe31a99ca335f829c2a5aad3c0b47844
Ryan Wise <ryanmwise1@gmail.com>
2015-10-11 12:56:22 -0700

    wip: change navigation to new-style functional component

--- browser_client/components/navigation.js
+++ browser_client/components/navigation.js
@@ -3,15 +3,11 @@ import PureComponent from 'react-pure-render/component';
 import { Link } from 'react-router';
 
 
-export default class Navigation extends PureComponent {
-    render () {
-        return (
-            <div className="links">
-                <ul>
-                    <li><Link to="/1" >Page One</Link></li>
-                    <li><Link to="/2">Page Two</Link></li>
-                </ul>
-            </div>
-        );
-    }
-}
+export default () => (
+    <div className="links">
+        <ul>
+            <li><Link to="/1" >Page One</Link></li>
+            <li><Link to="/2">Page Two</Link></li>
+        </ul>
+    </div>
+);
