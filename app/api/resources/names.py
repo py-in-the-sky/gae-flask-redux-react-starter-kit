c67d11d83795a3d7657c9eafcf157cb794a00960
Ryan Wise <ryanmwise1@gmail.com>
2015-12-05 13:21:38 -0800

    implemented more scalable Flask-RESTful project structure

---
+++ app/api/resources/names.py
@@ -0,0 +1,42 @@
+from flask.ext.restful import \
+    Resource, fields, marshal_with, reqparse, abort
+from datetime import datetime
+# from utils.werkzeug_debugger import werkzeug_debugger
+
+
+parser = reqparse.RequestParser(trim=True, bundle_errors=True)
+parser.add_argument(
+    'name',
+    type=str,
+    # location='json',
+    required=True,
+    help='"name" must be a string'
+)
+
+
+name_fields = {
+    'name': fields.String,
+    'now':  fields.DateTime
+}
+
+
+class Name(Resource):
+    decorators=[marshal_with(name_fields)]
+
+    def get(self):
+        # a = 4
+        # werkzeug_debugger()
+        return {
+            'name': 'World',
+            'foo': 'bar',
+            'now': datetime.now()
+        }
+
+    def post(self):
+        # abort(400, message='hi')
+        args = parser.parse_args()
+        return {
+            'name': args.name,
+            'foo': 'bar',
+            'now': datetime.now()
+        }
--- app/main.py
+++ app/main.py
@@ -2,22 +2,8 @@
 
 
 def create_app():
-    from flask import Flask, jsonify
+    from flask import Flask
     app = Flask(__name__)
-
-
-    @app.errorhandler(404)
-    def not_found(e):
-        """Return a custom 404 error."""
-        return jsonify({ 'message': 'Sorry, Nothing at this URL.' }), 404
-
-
-    @app.errorhandler(500)
-    def application_error(e):
-        """Return a custom 500 error."""
-        message = 'Sorry, unexpected error: {}'.format(e)
-        return jsonify({ 'message': message }), 500
-
 
     from api import api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
@@ -26,12 +12,12 @@ def create_app():
     if environ['SERVER_SOFTWARE'].startswith('Development'):
         from werkzeug.debug import DebuggedApplication
         app.debug = True
-        return DebuggedApplication(app, evalex=True)
+        app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
         # In order for debug to work with GAE, use DebuggedApplication.
-        # Note: We don't need to call run() since our application is
-        # embedded within the App Engine WSGI application server.
-    else:
-        return app
+
+    return app
 
 
 app = create_app()
+# Note: We don't need to call run() since our application is
+# embedded within the App Engine WSGI application server.
