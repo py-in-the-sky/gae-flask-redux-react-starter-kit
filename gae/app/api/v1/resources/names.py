53efcee682887244201c167d230e9f4d0afb247a
Ryan Wise <ryanmwise1@gmail.com>
2015-12-05 17:35:08 -0800

    WIP: Name model

--- gae/app/api/v1/resources/names.py
+++ gae/app/api/v1/resources/names.py
@@ -1,8 +1,9 @@
 from flask.ext.restful import \
     Resource, fields, marshal_with, reqparse, abort
 from datetime import datetime
-from app.models import name
-# from app.utils.werkzeug_debugger import werkzeug_debugger
+import random
+from app import models
+from app.utils.werkzeug_debugger import werkzeug_debugger
 
 
 parser = reqparse.RequestParser(trim=True, bundle_errors=True)
@@ -16,8 +17,8 @@ parser.add_argument(
 
 
 name_fields = {
-    'name': fields.String,
-    'now':  fields.DateTime
+    'name':     fields.String,
+    'created':  fields.DateTime
 }
 
 
@@ -25,19 +26,25 @@ class Name(Resource):
     decorators=[marshal_with(name_fields)]
 
     def get(self):
+        "return random Name"
         # a = 4
         # werkzeug_debugger()
+        name_keys = models.Name.query().fetch(keys_only=True)
+        name = random.choice(name_keys).get()
         return {
-            'name': 'World',
-            'foo': 'bar',
-            'now': datetime.now()
+            'name':    name.name,
+            'foo':     'bar',
+            'created': name.created
         }
 
     def post(self):
         # abort(400, message='hi')
+        # TODO: https://cloud.google.com/appengine/docs/python/ndb/modelclass#Model_get_or_insert
         args = parser.parse_args()
+        name = models.Name(name=args.name)
+        name.put()
         return {
-            'name': args.name,
-            'foo': 'bar',
-            'now': datetime.now()
+            'name':    name.name,
+            'foo':     'bar',
+            'created': name.created
         }
