6890c7c0911c06c2d0ca6470555789f7c9a687e0
Ryan Wise <ryanmwise1@gmail.com>
2015-12-07 09:28:06 -0800

    run lodash in `window` context while in `__TEST__` mode
    Using a copy of lodash that is run in the `window` context
    allows us to make use of sinon fake time and therefore control functions
    whose timing is regulated by certain lodash functions, such as
    `debounce` and `throttle`

--- browser_client/src/utils/lodash.js
+++ browser_client/src/utils/lodash.js
@@ -4,9 +4,29 @@ import isPlainObject from 'lodash/lang/isPlainObject';
 import debounce from 'lodash/function/debounce';
 
 
-export default {
+const defaultExport = {
     uniqueId,
     memoize,
     isPlainObject,
     debounce,
 };
+
+
+const makeExport = () => {
+    if (__TEST__) {
+        // Using a copy of lodash that's run in the `window` context
+        // allows us to make use of sinon's fake time and control functions
+        // whose timing is regulated by certain lodash functions, such as
+        // `debounce` and `throttle`.
+        const lodash = require('lodash').runInContext(window);
+        return Object.keys(defaultExport).reduce((obj, key) =>
+            ({ ...obj, [key]: lodash[key] })
+        , {});
+    }
+    else {
+        return defaultExport;
+    }
+};
+
+
+export default makeExport();
