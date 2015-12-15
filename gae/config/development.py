66447446405c75b3ea1eedc602777f222893b5e2
Ryan Wise <ryanmwise1@gmail.com>
2015-12-14 19:10:53 -0800

    ensure minimal datastore data for dev

--- gae/config/development.py
+++ gae/config/development.py
@@ -1,5 +1,6 @@
 from .config import Config
 from werkzeug.debug import DebuggedApplication
+from app.models.name import Name, root
 
 
 class Development(Config):
@@ -11,3 +12,9 @@ class Development(Config):
         app.debug = True
         app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
         # In order for debug to work with GAE, use DebuggedApplication.
+
+        for droid_name in ('bb8', 'r2d2', 'c3po'):
+            if Name.query(Name.name == droid_name).count() == 0:
+                Name(parent=root, name=droid_name).put()
+                # ensure minimal data for dev in datastore
+                # no need to worry about a transaction
