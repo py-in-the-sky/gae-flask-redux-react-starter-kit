15593226b9534d8b1639f10907e344dcc768ae8b
Ryan Wise <ryanmwise1@gmail.com>
2016-01-17 15:13:34 -0800

    WIP: make werkzeug-debugger response work

--- browser_client/src/middleware/api.js
+++ browser_client/src/middleware/api.js
@@ -23,7 +23,7 @@ export const beforeFetch = fetchCall => ({
 export const onFetchFail = (body, response, fetchCall, _action, dispatch) => {
     if (response.status >= 500) {
         if (__DEV__) {
-            require('../utils/devFetchDebug')(body, fetchCall.endpoint);
+            require('../utils/devFetchDebug')(body, response.url, fetchCall);
         }
 
         dispatch({ type: SERVER_ERROR });
--- browser_client/src/utils/devFetchDebug.js
+++ browser_client/src/utils/devFetchDebug.js
@@ -1,5 +1,17 @@
-const werkzeugDebugger = (flaskResponse, path) => {
-    window.history.pushState({}, 'Werkzeug Debugger', path);
+/*
+thanks to:
+    http://stackoverflow.com/a/3354511/1941513
+    http://ilee.co.uk/changing-url-without-page-refresh/
+    http://stackoverflow.com/a/4001415/1941513
+    http://stackoverflow.com/a/11933007/1941513
+    http://stackoverflow.com/a/3340186/1941513
+*/
+const werkzeugDebugger = (flaskResponse, url, fetchCall) => {
+    if (!sameOrigin(url)) return;
+
+    if(!confirm('Server Error!  Open Werkzeug Debugger?')) return;
+
+    window.history.pushState({}, 'Werkzeug Debugger', fetchCall.endpoint);
 
     try {
         window.document.open();
@@ -8,6 +20,16 @@ const werkzeugDebugger = (flaskResponse, path) => {
     finally {
         window.document.close();
     }
+};
+
+
+/*
+thanks to: https://gist.github.com/jlong/2428561
+*/
+const sameOrigin = url => {
+    const parser = document.createElement('a');
+    parser.href = url;
+    return parser.origin === window.location.origin;
 };
 
 
