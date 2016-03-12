09be8aa8e26ccd445fea17f751a14904ea1b8f2c
Ryan Wise <ryanmwise1@gmail.com>
2016-03-12 08:47:59 -0800

    add appstats

--- gae/appengine_config.py
+++ gae/appengine_config.py
@@ -10,6 +10,13 @@ from google.appengine.ext import vendor
 from os import path
 
 
+def webapp_add_wsgi_middleware(app):
+    # if os.getenv('SERVER_SOFTWARE', '').startswith('Dev'):
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
+
+
 # Third-party libraries are stored in "__app_env__", vendoring will make
 # sure that they are importable by the application.
 app_rootdir = path.dirname(__file__)
